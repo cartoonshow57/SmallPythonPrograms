4bb06cad37f012217582abfc9dc43fb6e648bb22
Lakshya <cartoonshow57@gmail.com>
2019-06-04 22:26:55 +0530

    Added new Program

---
+++ area_of_circle.py
@@ -0,0 +1,9 @@
+def area(radius):
+    pi = 3.14
+    area = pi * radius ** 2
+    return area
+
+
+radius = int(input("Enter the radius of the circle: "))
+print("The radius of the circle with radius",radius,"is",area(radius))
+input()
