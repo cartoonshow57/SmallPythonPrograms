515fbb88c1af32592da9a1f65d5c65328ff4d08d
Lakshya <cartoonshow57@gmail.com>
2019-06-05 21:59:09 +0530

    New Program

---
+++ array_rotation.py
@@ -0,0 +1,24 @@
+def array_rotation(arr, d):
+    test_arr = []
+    new_arr = []
+    for j in range(d):
+        element = arr.pop()
+        test_arr.append(element)
+        test_arr.sort()
+    new_arr = test_arr + arr
+    return new_arr
+
+
+arr = []
+
+num = int(input("Enter the number of elements in the array: "))
+
+for i in range(num):
+    arr.append(int(input("Enter an array number: ")))
+
+d = int(input("By how many elements should the array be rotated: "))
+
+print("The given array is:", arr)
+print("The array after rotation is:", array_rotation(arr, d))
+
+input()
