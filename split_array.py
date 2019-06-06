431e18f1268f40fc22ff79e66ce3a4a576076dac
Lakshya <cartoonshow57@gmail.com>
2019-06-06 20:49:56 +0530

    Adde new array Program

---
+++ split_array.py
@@ -0,0 +1,28 @@
+# This program splits a given array and adds the first part at the end
+
+def split_arr(arr, k):
+    new_arr = []
+    req_arr = []
+    count = 0
+    while (count < k):
+        element = arr.pop(0)
+        new_arr.append(element)
+        count += 1
+    req_arr = arr + new_arr
+    return req_arr
+
+
+arr = []
+
+n = int(input("Enter the number of elements to be put in the array: "))
+
+for i in range(n):
+    arr.append(int(input("Enter array element: ")))
+
+k = int(input("Enter from which element you want to split the array: "))
+
+print("The given array was:", arr)
+
+print("The array after split comes out as:", split_arr(arr, k))
+
+input()
