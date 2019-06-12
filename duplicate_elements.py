505fda4a09fc408557c733e2db4b86c7eac809e5
Lakshya <cartoonshow57@gmail.com>
2019-06-12 17:06:36 +0530

    Element Swap Program

---
+++ duplicate_elements.py
@@ -0,0 +1,21 @@
+"""This program counts the elements of a list
+If all the elements are unique it returns False
+and is elements occur twice it returns True"""
+
+def element_count(arr):
+    for i in arr:
+        if (arr.count(i) != 2):
+            return False
+    return True
+
+
+arr = []
+
+num = int(input("Enter the number of elements in the array: "))
+
+for j in range(num):
+    arr.append(int(input("Enter array element: ")))
+
+print(element_count(arr))
+
+input()
