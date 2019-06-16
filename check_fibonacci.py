c0c65fb6f877d170723d6439975b8b4c7fb95154
Lakshya <cartoonshow57@gmail.com>
2019-06-16 14:57:37 +0530

    Modified if statement

--- check_fibonacci.py
+++ check_fibonacci.py
@@ -1,17 +1,18 @@
 # This program checks if a number belongs to the Fibonacci series
 
+
 def check_perf_square(number):
     s = int(number ** 0.5)
     return s * s == number
 
 
-def check_fib(num):
-    return check_perf_square(5 * num * num + 4) or check_perf_square(5 * num * num - 4)
+def check_fib(num1):
+    return check_perf_square(5 * num1 * num1 + 4) or check_perf_square(5 * num1 * num1 - 4)
 
 
 num = int(input("Enter a number to check if it's Fibonacci or not: "))
 
-if check_fib(num) == True:
+if check_fib(num):
     print(num, "is a Fibonacci number.")
 else:
     print(num, "is not a Fibonacci number.")
