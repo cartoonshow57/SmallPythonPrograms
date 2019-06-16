78fcd975e4f22c64f194b75d14eb263218e08c99
Lakshya <cartoonshow57@gmail.com>
2019-06-16 14:46:08 +0530

    Modified inner scope variable

--- multiples_of_num.py
+++ multiples_of_num.py
@@ -1,12 +1,13 @@
 """This program takes a positive integer
 and returns a list that contains first five multiples of that number"""
 
-def list_of_multiples(num):
-    Lst = []
+
+def list_of_multiples(num1):
+    lst = []
     for i in range(1, 6):
-        multiple = num * i
-        Lst.append(multiple)
-    return Lst
+        multiple = num1 * i
+        lst.append(multiple)
+    return lst
 
 
 num = int(input("Enter a positive number: "))
