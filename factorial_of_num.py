498e8bfb88175750f5e5052324fded4ac66bd27f
Lakshya <cartoonshow57@gmail.com>
2019-06-16 14:51:15 +0530

    Added additional lines and missing whitespace

--- factorial_of_num.py
+++ factorial_of_num.py
@@ -1,9 +1,13 @@
 # This program inputs a number and prints its factorial
 
+
 def fact_of_num(num):
     if num == 0 or num == 1:
         return 1
     else:
         return num * fact_of_num(num - 1)
-print("The factorial of 5 is ",fact_of_num(5))
+
+
+print("The factorial of 5 is ", fact_of_num(5))
+
 input()
