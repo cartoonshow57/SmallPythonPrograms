1e987de1bdbab315cdb506a3741a01ff7b94196e
Lakshya <cartoonshow57@gmail.com>
2019-06-16 15:02:36 +0530

    Added whitespace and some name changes

--- area_of_circle.py
+++ area_of_circle.py
@@ -1,11 +1,12 @@
 # This program takes radius as input and calculates the area
 
-def area(radius):
+
+def area(radius1):
     pi = 3.14
-    area = pi * radius ** 2
-    return area
+    area1 = pi * radius1 ** 2
+    return area1
 
 
 radius = int(input("Enter the radius of the circle: "))
-print("The radius of the circle with radius",radius,"is",area(radius))
+print("The radius of the circle with radius", radius, "is", area(radius))
 input()
