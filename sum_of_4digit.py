0e496d94079360fdb2f72dd634e1f25b73f4e34d
Lakshya <cartoonshow57@gmail.com>
2019-06-16 14:34:20 +0530

    Added line after comment

--- sum_of_4digit.py
+++ sum_of_4digit.py
@@ -1,5 +1,6 @@
 # This program inputs a number and prints the sum of its digits
 
+
 num = eval(input("Enter a 4-digit number : "))
 r1 = num % 10
 q1 = num // 10
@@ -10,4 +11,5 @@ q3 = q2 // 10
 r4 = q3
 sum_num = r1 + r2 + r3 + r4
 print("The sum of the digits is :-\n", r1, "\n", r2, "\n", r3, "\n", r4, "\n", sum_num)
+
 input()
