b9498f470e23e38e7598d77af40d77caa993fcad
Cartoon <cartoonshow57@gmail.com>
2020-05-12 18:37:28 +0530

    Added insertion sort

---
+++ insertion_sort.py
@@ -0,0 +1,24 @@
+"""
+Implementation of Insertion sort
+"""
+
+
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        currentvalue = arr[i]
+        position = i
+
+        while position > 0 and arr[position - 1] > currentvalue:
+            arr[position] = arr[position - 1]
+            position -= 1
+
+        arr[position] = currentvalue
+    return array
+
+
+array = list(map(int, input('Enter array elements: ').strip().split()))
+
+print('Given array: ', array)
+print("Sorted array: ", insertion_sort(array))
+
+input()
